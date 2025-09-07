3acb3c6f62f6c882b9f84ca357b4346397f73d6d
SamYuan1990 <yy19902439@126.com>
2025-09-07 10:55:47 +0800

    fix up
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

--- App/chatmessage.py
+++ App/chatmessage.py
@@ -1,10 +1,12 @@
 import flet as ft
 
+
 class Message:
     def __init__(self, user_name: str, text: str, message_type: str):
         self.user_name = user_name
         self.text = text
         self.message_type = message_type
+
 
 class ChatMessage(ft.Row):
     def __init__(self, message: Message):
